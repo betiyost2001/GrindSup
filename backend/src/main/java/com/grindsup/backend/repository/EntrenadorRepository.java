205b8cba0c13707a8d8a62651dc5e247dae68e46
azuCan <402061@sistemas.frc.utn.edu.ar>
2025-08-31 05:21:32 -0300

    Actualización del backend sin errores

--- backend/src/main/java/com/grindsup/backend/repository/EntrenadorRepository.java
+++ backend/src/main/java/com/grindsup/backend/repository/EntrenadorRepository.java
@@ -1,10 +1,8 @@
-package com.grinsup.backend.repository;
+package com.grindsup.backend.repository;
 
 import com.grindsup.backend.model.Entrenador;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface EntrenadorRepository extends JpaRepository<Entrenador, Long> {
 
 }
