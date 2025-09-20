560dc8849cbad93f968a00206cc61e772c5dc82c
betiyost2001 <yostbetina20@gmail.com>
2025-09-20 11:59:09 -0300

    HU4: Listar alumnos con búsqueda, cambio de estado y eliminación

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,26 @@
 import { Box, Container } from "@chakra-ui/react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import RegistrarAlumnoForm from "./pages/Alumno/RegistrarAlumnoForm";
+import AlumnoList from "./components/AlumnoList";
 
 export default function App() {
-  
   return (
-    <Box minH="100vh" display="flex" flexDirection="column">
-      <Header />
-      <Box as="main" flex="1" py={{ base: 6, md: 10 }}>
-        <Container maxW="container.md">
-          <RegistrarAlumnoForm />
-        </Container>
+    <BrowserRouter>
+      <Box minH="100vh" display="flex" flexDirection="column">
+        <Header />
+        <Box as="main" flex="1" py={{ base: 6, md: 10 }}>
+          <Container maxW="container.xl">
+            <Routes>
+              <Route path="/" element={<AlumnoList />} />
+              <Route path="/alumno/registrar" element={<RegistrarAlumnoForm />} />
+            </Routes>
+          </Container>
+        </Box>
+        <Footer />
       </Box>
-      <Footer />
-    </Box>
+    </BrowserRouter>
   );
 }
+
